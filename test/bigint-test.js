c4e26ed01bd991b16862ce52da79afc93b84a7da
Ben Adida <ben@adida.net>
2013-03-16 11:42:06 -0700

    one more bigint test

--- test/bigint-test.js
+++ test/bigint-test.js
@@ -13,6 +13,16 @@ suite.addBatch({
     "looks good": function(bi) {
       assert.isObject(bi);
     }
+  },
+  "add bigints": {
+    topic: function() {
+      var a = new BigInt("10000000000000000000000000004", 10);
+      var b = new BigInt("20000000000000040000000000008", 10);
+      return a.add(b);
+    },
+    "gives the right result": function(sum) {
+      assert.ok(sum.equals(new BigInt("30000000000000040000000000012", 10)));
+    }
   }
 });
 
